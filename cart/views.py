c99e964d8a4d6f92776831621c890115ac06271a
ramaminiaina <mamy@aims.ac.za>
2024-07-22 20:18:25 +0200

    show cart item from db

--- cart/views.py
+++ cart/views.py
@@ -6,10 +6,49 @@ from django.db import transaction
 from .models import Cart 
 
 
-
+@login_required(login_url='signin')
 def cart_view(request):
 
-    return render(request, 'cart/cart.html')
+    cart_total_amount = 0
+    fee = 1.5
+    
+    if 'cart_data_obj' in request.session:
+        del request.session['cart_data_obj']
+    
+    try:
+        cart_items = Cart.objects.filter(user=request.user)
+        cart_data_obj = {}
+        for item in cart_items:
+            cart_data_obj[item.product_id] = {
+                'product_title': item.product_title,
+                'quantity': item.product_quantity,
+                'product_price': item.product_price,
+                'image': item.product_image,
+                'category': item.product_category,
+            }
+        
+        request.session['cart_data_obj'] = cart_data_obj
+
+        for product_id, item in cart_data_obj.items():
+
+            subtotal = round(int(item['quantity']) * float(item['product_price']), 2)
+            item['subtotal'] = subtotal
+            cart_total_amount += subtotal
+
+        cart_grand_total = round((cart_total_amount + (cart_total_amount * fee)/100), 2)
+
+
+    except Cart.DoesNotExist:
+        cart_items = None
+
+    context = {
+            'cart_total_amount': round(cart_total_amount, 2),
+            'cart_data': cart_data_obj,
+            'cart_grand_total': cart_grand_total,
+            'total_item': len(cart_data_obj),
+        }
+
+    return render(request, 'cart/cart.html', context)
 
 
 # @login_required(login_url='signin')
@@ -62,7 +101,7 @@ def add_to_cart(request):
                     cart.product_quantity += quantity
                     cart.product_subtotal = subtotal
                     cart.save()
-
+            print(request.session['cart_data_obj'])
             return JsonResponse({
                 'bool': True,
                 'data': request.session['cart_data_obj'],
